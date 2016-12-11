35e6572eb0f8b891ba1326213bf8bb5f94cb68f9
Joe Capezzuto <joseph.capezzuto@gmail.com>
2016-12-10 19:59:20 -0800

    [env] Add d3 and body-parser modules

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
 
 var PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use('/', express.static(__dirname + '/src'));
 app.use('/dist',express.static(__dirname + '/dist'));
+
+app.get('/data/:visId', function(req, res) {
+  console.log('visId =', visId);
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
