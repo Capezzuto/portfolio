4a9a6c266d9d6e589bd7d4e5fb21a93283343e65
Joe Capezzuto <joseph.capezzuto@gmail.com>
2016-12-21 22:23:56 -0800

    [refactor] Convert switch statements in box office scraper to object containing functions

--- server/scraper/scrape-bom-mongo.js
+++ server/scraper/scrape-bom-mongo.js
@@ -1,15 +1,13 @@
 const cheerio = require('cheerio');
 const request = require('request');
-const moment = require('moment');
+// const moment = require('moment');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 const BoxOfficeWeekly = require('../boxOffice/boxOfficeWeeklyModel.js');
 const BoxOfficeDaily = require('../boxOffice/boxOfficeDailyModel.js');
-// const fs = require('fs');
-// const currWeek = require('./week');
-// const weeklyDataFile = require('./weekly-data');
-// const dailyDataFile = require('./daily-data');
+
 const args = process.argv.slice(2);
+
 mongoose.connect('mongodb://localhost/localdb');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error opening connection'));
@@ -29,7 +27,7 @@ db.on('open', () => {
 
 module.exports = {
   getWeeklyData: function(arg) {
-    console.log('in getWeeklyData....');
+    // console.log('in getWeeklyData....');
     request(`http://www.boxofficemojo.com/weekly/chart/?yr=${arg.year}&wk=${arg.week}&p=.htm`, (error, response, body) => {
       if (error) {
         console.log('Error: ', error);
@@ -49,6 +47,8 @@ module.exports = {
                       }).get();
 
       let week = `${arg.year}_${arg.week}`;
+      console.log(`weeeeeeeeeeeeek is ${week}`);
+
       let total_gross = $('#body')
                           .find('tr')
                           .last()
@@ -59,26 +59,31 @@ module.exports = {
                           .eq(1).text();
       total_gross = Number(total_gross.replace(/\D/g, ''));
 
-      let jsonData = weeklyData.reduce(function(obj, curr, i) {
+      const reducerFuncs = {
+        0: function(obj, curr, entry) { obj.movies[entry] = { rank: Number(curr) }; },
+        1: function(obj, curr, entry) { obj.movies[entry]['prev_rank'] = Number(curr) || 0; },
+        2: function(obj, curr, entry) { obj.movies[entry]['title'] = curr; },
+        3: function(obj, curr, entry) { obj.movies[entry]['studio'] = curr; },
+        4: function(obj, curr, entry) { obj.movies[entry]['week_gross'] = Number(curr.replace(/\D/g, '')); },
+        5: function(obj, curr, entry) { obj.movies[entry]['pct_change'] = parseInt(curr.replace(/\,/g, ''), 10) || 0;},
+        6: function(obj, curr, entry) { obj.movies[entry]['theaters'] = Number(curr.replace(/\D/g, '')); },
+        7: function(obj, curr, entry) { obj.movies[entry]['theater_change'] = parseInt(curr.replace(/\,/g, ''), 10) || 0; },
+        8: function(obj, curr, entry) { obj.movies[entry]['avg'] = Number(curr.replace(/\D/g, '')); },
+        9: function(obj, curr, entry) { obj.movies[entry]['total'] = Number(curr.replace(/\D/g, '')); },
+        10: function(obj, curr, entry) { obj.movies[entry]['budget'] = Number(curr.replace(/\$/g, '')) || 0; },
+        11: function(obj, curr, entry) { obj.movies[entry]['week'] = Number(curr) || 1; },
+      }
+
+      // console.log(`weeklyData is ${weeklyData}`)
+
+      let jsonData = weeklyData.reduce((obj, curr, i) => {
         let x = i % 12;
         let entry = Math.floor(i/12);
-        switch(x) {
-          case 0: obj.movies[entry] = { rank: Number(curr) }; break;
-          case 1: obj.movies[entry]['prev_rank'] = Number(curr) || 0; break;
-          case 2: obj.movies[entry]['title'] = curr; break;
-          case 3: obj.movies[entry]['studio'] = curr; break;
-          case 4: obj.movies[entry]['week_gross'] = Number(curr.replace(/\D/g, '')); break;
-          case 5: obj.movies[entry]['pct_change'] = parseInt(curr.replace(/\,/g, ''), 10) || 0; break;
-          case 6: obj.movies[entry]['theaters'] = Number(curr.replace(/\D/g, '')); break;
-          case 7: obj.movies[entry]['theater_change'] = parseInt(curr.replace(/\,/g, ''), 10) || 0; break;
-          case 8: obj.movies[entry]['avg'] = Number(curr.replace(/\D/g, '')); break;
-          case 9: obj.movies[entry]['total'] = Number(curr.replace(/\D/g, '')); break;
-          case 10: obj.movies[entry]['budget'] = Number(curr.replace(/\$/g, '')) || 0; break;
-          case 11: obj.movies[entry]['week'] = Number(curr); break;
-          default: break;
-        }
+        reducerFuncs[x](obj, curr, entry);
         return obj;
       }, { week, total_gross, movies: [] });
+
+      // console.log(`******* ---------> ${jsonData.movies[2].week}`);
 
       BoxOfficeWeekly.create(jsonData)
         .then((output) => {
@@ -96,66 +101,54 @@ module.exports = {
   getDailyData: function(arg) {
     console.log('in getDailyData...');
     let dailyData;
-    request(`http://www.boxofficemojo.com/daily/chart/?sortdate=${arg.date}&view=1day&p=.htm`, (error, response, body) => {
-      if(error) {
-        console.log(error);
-      }
-      console.log('status code:', response.statusCode);
-
-      const $ = cheerio.load(body);
-
-      dailyData = $('#body')
-                    .find('center')
-                    .find('table').slice(2,3)
-                    .find('tr').slice(1)
-                    .find('td')
-                    .map(function(i, el) {
-                      if (i < 110) return $(el).text();
-                    }).get();
-
-      let jsonData = dailyData.reduce((obj, curr, i) => {
-        let x = i % 11;
-        let entry = Math.floor(i/11);
-        switch(x) {
-          case 0: obj.top10[entry] = { rank_today: Number(curr) }; break;
-          case 1: obj.top10[entry]['rank_yesterday'] = Number(curr) || 0; break;
-          case 2: obj.top10[entry]['title'] = curr; break;
-          case 3: obj.top10[entry]['studio'] = curr; break;
-          case 4: obj.top10[entry]['daily_gross'] = Number(curr.replace(/\D/g, '')); break;
-          case 5: obj.top10[entry]['pct_change_yd'] = parseInt(curr.replace(/\,/g, ''), 10) || 0; break;
-          case 6: obj.top10[entry]['pct_change_lw'] = parseInt(curr.replace(/\,/g, ''), 10) || 0; break;
-          case 7: obj.top10[entry]['theaters'] = Number(curr.replace(/\D/g, '')); break;
-          case 8: obj.top10[entry]['avg'] = Number(curr.replace(/\D/g, '')); break;
-          case 9: obj.top10[entry]['total'] = Number(curr.replace(/\D/g, '')); break;
-          case 10: obj.top10[entry]['day'] = Number(curr); break;
-          default: break;
+    request(`http://www.boxofficemojo.com/daily/chart/?sortdate=${arg.date}&view=1day&p=.htm`,
+      (error, response, body) => {
+        if(error) {
+          console.log(error);
         }
-        return obj;
-      }, { date: arg.date, week: arg.week, top10: [] });
+        console.log('status code:', response.statusCode);
 
-      BoxOfficeDaily.create(jsonData)
-        .then((output) => {
-          console.log('created...', output);
-          mongoose.disconnect();
-        })
-        .catch((err) => {
-          console.log('error', err);
-          mongoose.disconnect();
-        });
+        const $ = cheerio.load(body);
+
+        dailyData = $('#body')
+                      .find('center')
+                      .find('table').slice(2,3)
+                      .find('tr').slice(1)
+                      .find('td')
+                      .map(function(i, el) {
+                        if (i < 110) return $(el).text();
+                      }).get();
+
+        const reducerFuncs = {
+          0: function(obj, curr, entry) { obj.top10[entry] = { rank_today: Number(curr) }; },
+          1: function(obj, curr, entry) { obj.top10[entry]['rank_yesterday'] = Number(curr) || 0; },
+          2: function(obj, curr, entry) { obj.top10[entry]['title'] = curr; },
+          3: function(obj, curr, entry) { obj.top10[entry]['studio'] = curr; },
+          4: function(obj, curr, entry) { obj.top10[entry]['daily_gross'] = Number(curr.replace(/\D/g, '')); },
+          5: function(obj, curr, entry) { obj.top10[entry]['pct_change_yd'] = parseInt(curr.replace(/\,/g, ''), 10) || 0; },
+          6: function(obj, curr, entry) { obj.top10[entry]['pct_change_lw'] = parseInt(curr.replace(/\,/g, ''), 10) || 0; },
+          7: function(obj, curr, entry) { obj.top10[entry]['theaters'] = Number(curr.replace(/\D/g, '')); },
+          8: function(obj, curr, entry) { obj.top10[entry]['avg'] = Number(curr.replace(/\D/g, '')); },
+          9: function(obj, curr, entry) { obj.top10[entry]['total'] = Number(curr.replace(/\D/g, '')); },
+          10: function(obj, curr, entry) { obj.top10[entry]['day'] = Number(curr) || 1; },
+        };
+
+        let jsonData = dailyData.reduce((obj, curr, i) => {
+          let x = i % 11;
+          let entry = Math.floor(i/11);
+          reducerFuncs[x](obj, curr, entry);
+          return obj;
+        }, { date: arg.date, week: arg.week, top10: [] });
+
+        BoxOfficeDaily.create(jsonData)
+          .then((output) => {
+            console.log('created...', output);
+            mongoose.disconnect();
+          })
+          .catch((err) => {
+            console.log('error', err);
+            mongoose.disconnect();
+          });
     })
   }
-}
-
-function _writeToFile(fileName, oldData, newData, time, year) {
-  let keystring = year ? `${year}_${time}` : time;
-  if (typeof oldData != 'object') {
-    console.error('the type of data supplied is wrong')
-    throw err;
-  }
-  oldData[keystring] = newData;
-  fs.writeFile(fileName, JSON.stringify(oldData, null, 2), (err) => {
-    if (err) console.log(err);
-    console.log(`wrote to ${fileName}`);
-  });
-
-}
+};
